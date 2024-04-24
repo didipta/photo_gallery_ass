4e33a7c9368e154b80be34f27715202adf88022b
didipta <66532706+didipta@users.noreply.github.com>
2024-04-24 21:26:36 +0600

    Initial commit

---
+++ lib/Component/PhotoGallery.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class PhotoGallery extends StatefulWidget {
+  const PhotoGallery({Key? key}) : super(key: key);
+
+  @override
+  State<PhotoGallery> createState() => _PhotoGalleryState();
+}
+
+class _PhotoGalleryState extends State<PhotoGallery> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
